d6fdac35a00f09bf838f81876effd18dff5503ce
s0uravhald3r <bishalder96@gmail.com>
2022-07-26 14:13:01 +0530

    bug fixed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# search_user
+# User Details
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'User',
+        title: 'User Details',
         theme: ThemeData(
           primarySwatch: Colors.amber,
         ),
